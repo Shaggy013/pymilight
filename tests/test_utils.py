bf50c35bbf3637b48420cb38caed453c6fd8174b
David Kent <davidkent@fastmail.com.au>
2018-07-10 21:02:23 +1000

    Get mired conversion round the right way.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,25 +13,25 @@ class UtilsTestCase(unittest.TestCase):
         self.assertAlmostEqual(6500, utils.mireds_to_kelvin(153.828), -1)
 
     def testMiredsToWhite(self):
-        self.assertAlmostEqual(0, utils.mireds_to_white_val(370))
-        self.assertAlmostEqual(100, utils.mireds_to_white_val(153))
+        self.assertAlmostEqual(100, utils.mireds_to_white_val(370))
+        self.assertAlmostEqual(0, utils.mireds_to_white_val(153))
 
         self.assertAlmostEqual(50, utils.mireds_to_white_val(262))
 
     def testWhiteToMireds(self):
-        self.assertAlmostEqual(370, utils.white_val_to_mireds(0))
-        self.assertAlmostEqual(153, utils.white_val_to_mireds(100))
+        self.assertAlmostEqual(370, utils.white_val_to_mireds(100))
+        self.assertAlmostEqual(153, utils.white_val_to_mireds(0))
 
         self.assertAlmostEqual(262, utils.white_val_to_mireds(50))
 
     def testKelvinToWhite(self):
-        self.assertAlmostEqual(0, utils.kelvin_to_white_val(2700))
-        self.assertAlmostEqual(100, utils.kelvin_to_white_val(6500))
+        self.assertAlmostEqual(100, utils.kelvin_to_white_val(2700))
+        self.assertAlmostEqual(0, utils.kelvin_to_white_val(6500))
 
         self.assertAlmostEqual(50, utils.kelvin_to_white_val(3817))
 
     def testWhiteToKelvin(self):
-        self.assertAlmostEqual(2700, utils.white_val_to_kelvin(0), -1)
-        self.assertAlmostEqual(6500, utils.white_val_to_kelvin(99.8), -2)
+        self.assertAlmostEqual(6500, utils.white_val_to_kelvin(0), -2)
+        self.assertAlmostEqual(2700, utils.white_val_to_kelvin(99.8), -1)
 
         self.assertAlmostEqual(3820, utils.white_val_to_kelvin(50), 0)
